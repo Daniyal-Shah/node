af699bc5407f42941bddf870ef0daa0677f77b3b
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-03-06 17:12:28 +0500

    winston-mongodb(error log on server/database side)

--- handling-errors/src/middlewares/error.js
+++ handling-errors/src/middlewares/error.js
@@ -1,8 +1,38 @@
 const winston = require("winston");
+
+// Import mongodb
+require("winston-mongodb");
+const { createLogger, format, transports } = require("winston");
+
 const logConfiguration = {
   transports: [
-    new winston.transports.File({
+    // File transport
+    new transports.File({
       filename: "logFile.log",
+      format: format.combine(
+        format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+        format.align(),
+        format.printf(
+          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+        )
+      ),
+    }),
+
+    // MongoDB transport
+    new transports.MongoDB({
+      level: "error",
+      //mongo database connection link
+      db: "mongodb://localhost:27017/auth",
+      options: {
+        useUnifiedTopology: true,
+      },
+      // A collection to save json formatted logs
+      collection: "error_logs",
+      format: format.combine(
+        format.timestamp(),
+        // Convert logs to a json format
+        format.json()
+      ),
     }),
   ],
 };
