a0c2d0c8f575f0ab286244a39d0d826bd1f53f94
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-03-06 21:36:20 +0500

    tests for lib absolute method

--- unit-testing/tests/lib.test.js
+++ unit-testing/tests/lib.test.js
@@ -1,3 +1,15 @@
-test("first test", () => {
-  throw new Error("something failed");
+const lib = require("../lib");
+test("absolute test positive", () => {
+  const result = lib.absolute(2);
+  expect(result).toBe(2);
+});
+
+test("absolute test negative", () => {
+  const result = lib.absolute(-2);
+  expect(result).toBe(3);
+});
+
+test("absolute test 0", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
 });
