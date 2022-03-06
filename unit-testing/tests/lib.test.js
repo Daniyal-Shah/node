d8c23bc2c63c4f8a87158c6b8f95a28e6a832068
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-03-06 21:37:26 +0500

    grouping tests

--- unit-testing/tests/lib.test.js
+++ unit-testing/tests/lib.test.js
@@ -1,15 +1,18 @@
 const lib = require("../lib");
-test("absolute test positive", () => {
-  const result = lib.absolute(2);
-  expect(result).toBe(2);
-});
 
-test("absolute test negative", () => {
-  const result = lib.absolute(-2);
-  expect(result).toBe(3);
-});
+describe("absolute", () => {
+  test("positive", () => {
+    const result = lib.absolute(2);
+    expect(result).toBe(2);
+  });
 
-test("absolute test 0", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  test("negative", () => {
+    const result = lib.absolute(-2);
+    expect(result).toBe(3);
+  });
+
+  test("0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
