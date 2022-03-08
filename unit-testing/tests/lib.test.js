017c7a9e9e0b5a11c892a34185be8d650c968c4d
Daniyal-Shah <daniyalshah7862018@gmail.com>
2022-03-08 20:59:44 +0500

    not so important

--- unit-testing/tests/lib.test.js
+++ unit-testing/tests/lib.test.js
@@ -8,11 +8,19 @@ describe("absolute", () => {
 
   test("negative", () => {
     const result = lib.absolute(-2);
-    expect(result).toBe(3);
+    expect(result).toBe(2);
   });
 
   test("0", () => {
     const result = lib.absolute(0);
     expect(result).toBe(0);
+  });
+});
+
+describe("greeting", () => {
+  test("daniyal ", () => {
+    const result = lib.greet("daniyal !");
+    expect(result).toMatch(/daniyal/);
+    expect(result).toContain("daniyal");
   });
 });
